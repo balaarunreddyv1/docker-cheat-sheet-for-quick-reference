b9add77a789ab173beff819d5c91ee361bd66737
Arun <balaarun.vatti@appdirect.com>
2019-10-16 16:30:37 -0400

    Create learn.sh

---
+++ learn.sh
@@ -0,0 +1,9 @@
+# Udemy-Docker-mastery my cheatsheet
+https://www.udemy.com/course/docker-mastery/
+
+
+- whats going on in the container
+
+docker container top - process list in one container
+docker container inspect - details of one container config
+docker container stats - perforamce stats for all the containers
